5e5d3a162cd5eab7c646a9adcd3fa0025ed327e0
DARKROSS <andresarcangel00@gmail.com>
2025-09-13 16:45:56 -0500

    empty, notfound, auth

--- src/app/pages/auth/auth.routes.ts
+++ src/app/pages/auth/auth.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { Access } from './access';
-import { Login } from './login';
-import { Error } from './error';
+import { Access } from './access/access';
+import { Login } from './login/login';
+import { Error } from './error/error';
 
 export default [
     { path: 'access', component: Access },
--- src/app/pages/empty/empty.ts
+++ src/app/pages/empty/empty.ts
@@ -3,9 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-empty',
     standalone: true,
-    template: ` <div class="card">
-        <div class="font-semibold text-xl mb-4">Empty Page</div>
-        <p>Use this page to start from scratch and place your custom content.</p>
-    </div>`
+    templateUrl: './empty.html',
+    styleUrls: ['./empty.css']
 })
 export class Empty {}
