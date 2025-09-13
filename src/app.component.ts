399c74c5613b6c1977f2d81f98486823eee15f82
DARKROSS <andresarcangel00@gmail.com>
2025-09-13 17:19:04 -0500

    migración del app.component

--- src/app.component.ts
+++ src/app.component.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
     selector: 'app-root',
     standalone: true,
     imports: [RouterModule],
-    template: `<router-outlet></router-outlet>`
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent { }
